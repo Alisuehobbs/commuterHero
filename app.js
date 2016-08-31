b22be4f5099ac8868d31f5ec7bcaf9c74e1268a6
Ali Hobbs <alisuehobbs@gmail.com>
2016-08-31 13:57:45 -0600

    pretty sure the stats calculator for all modes of transportation are working right

--- app.js
+++ app.js
@@ -1,100 +1,115 @@
 $(document).ready(function() {
 
-  // modals
-  $('.modal-trigger').leanModal();
+            // modals
+            $('.modal-trigger').leanModal();
 
-  //form select
-  $('select').material_select();
+            //form select
+            $('select').material_select();
 
-    // submit
-    $('#submit').on('click', function() {
-      //clear directions
-      $('#directions').children().remove()
+            // submit
+            $('#submit').on('click', function() {
+                    //clear directions
+                    $('#directions').children().remove()
 
-      var startVal = $('#start').val()
-      var endVal = $('#end').val()
-      var days = $('#days').val()
-      var typeOfTranspo = $('#typeOfTranspo').val()
+                    var startVal = $('#start').val()
+                    var endVal = $('#end').val()
+                    var days = $('#days').val()
+                    var typeOfTranspo = $('#typeOfTranspo').val()
 
-        $('#map').show()
-        $('.showDirections').show()
-        $('.hiddenCards').show()
+                    $('#map').show()
+                    $('.showDirections').show()
+                    $('.hiddenCards').show()
 
-        $('.showDirections').click(function() {
-            $('#directions').show();
-            $('.showDirections').hide();
-            $('.hideDirections').show();
-        })
+                    $('.showDirections').click(function() {
+                        $('#directions').show();
+                        $('.showDirections').hide();
+                        $('.hideDirections').show();
+                    })
 
-        $('.hideDirections').click(function() {
-          $('#directions').hide();
-          $('.showDirections').show();
-          $('.hideDirections').hide();
-        })
+                    $('.hideDirections').click(function() {
+                        $('#directions').hide();
+                        $('.showDirections').show();
+                        $('.hideDirections').hide();
+                    })
 
-        //stats info
-        $('#statsInfo').text(`If you travel by ${typeOfTranspo} for ${days} days this week, you will save:`)
+                    //stats info
+                    $('#statsInfo').text(`If you travel by ${typeOfTranspo} for ${days} days this week, you will save:`)
 
-        //map and directions
-        function initMap() {
-            var map = new google.maps.Map(document.getElementById('map'), {
-                zoom: 10,
-                center: {
-                    lat: 40.02,
-                    lng: 105.27
-                } // centralized on Boulder
-            });
+                    //map and directions
+                    function initMap() {
+                        var map = new google.maps.Map(document.getElementById('map'), {
+                            zoom: 10,
+                            center: {
+                                lat: 40.02,
+                                lng: 105.27
+                            } // centralized on Boulder
+                        });
 
-            var directionsService = new google.maps.DirectionsService;
-            var directionsDisplay = new google.maps.DirectionsRenderer({
-                draggable: true,
-                map: map,
-                panel: document.getElementById('directions')
-            });
+                        var directionsService = new google.maps.DirectionsService;
+                        var directionsDisplay = new google.maps.DirectionsRenderer({
+                            draggable: true,
+                            map: map,
+                            panel: document.getElementById('directions')
+                        });
 
-            directionsDisplay.addListener('directions_changed', function() {
-                computeTotalDistance(directionsDisplay.getDirections());
-            });
+                        directionsDisplay.addListener('directions_changed', function() {
+                            computeTotalDistance(directionsDisplay.getDirections());
+                        });
 
-            displayRoute(startVal, endVal, directionsService,
-                directionsDisplay);
-        }
-        initMap()
+                        displayRoute(startVal, endVal, directionsService,
+                            directionsDisplay);
+                    }
+                    initMap()
 
-        function displayRoute(origin, destination, service, display) {
-            service.route({
-                origin: origin,
-                destination: destination,
-                travelMode: typeOfTranspo,
-            }, function(response, status) {
-                if (status === 'OK') {
-                    display.setDirections(response);
-                } else {
-                    alert('Could not display directions due to: ' + status);
-                }
-            });
-        }
+                    function displayRoute(origin, destination, service, display) {
+                        service.route({
+                            origin: origin,
+                            destination: destination,
+                            travelMode: typeOfTranspo,
+                        }, function(response, status) {
+                            if (status === 'OK') {
+                                display.setDirections(response);
+                            } else {
+                                alert('Could not display directions due to: ' + status);
+                            }
+                        });
+                    }
 
-        function computeTotalDistance(result) {
-            var total = 0;
-            var myroute = result.routes[0];
-            for (var i = 0; i < myroute.legs.length; i++) {
-                total += myroute.legs[i].distance.value;
-            }
-            miles = ((total / 1609.344) * 2).toFixed(2);
-            $('#distance').text(miles)
+                    function computeTotalDistance(result) {
+                        var total = 0;
+                        var myroute = result.routes[0];
+                        for (var i = 0; i < myroute.legs.length; i++) {
+                            total += myroute.legs[i].distance.value;
+                        }
 
-            gallons = ((miles / 19.5) * days).toFixed(2)
-            $('#gallons').text(gallons)
+                        miles = ((total / 1609.344) * 2).toFixed(2);
+                        $('#distance').text(miles)
 
-            dollars = ((gallons * 2.218) * days).toFixed(2)
-            $('#dollars').text('$' + dollars)
+                        function statCalculation(typeOfTranspo) {
+                            if (typeOfTranspo === 'TRANSIT') {
+                              busGallons = ((miles/3.26)/35)
+                              gallons = (((miles / 21.6)- busGallons) * days).toFixed(2)
+                              $('#gallons').text(gallons)
 
-            emissions = ((gallons * 8887) * days).toFixed(2)
-            $('#emissions').text(emissions)
+                              dollars = ((gallons * 2.218) * days).toFixed(2)
+                              $('#dollars').text('$' + dollars)
 
+                              dollars = ((gallons * 2.218) * days).toFixed(2)
+                              $('#dollars').text('$' + dollars)
 
-        }
-    })
+                            } else {
+                                gallons = ((miles / 21.6) * days).toFixed(2)
+                                $('#gallons').text(gallons)
 
-})
+                                dollars = ((gallons * 2.218) * days).toFixed(2)
+                                $('#dollars').text('$' + dollars)
+
+                                emissions = ((gallons * 8887) * days).toFixed(2)
+                                $('#emissions').text(emissions)
+                              }
+                            }
+                          statCalculation(typeOfTranspo)
+                        }
+                    })
+
+            })

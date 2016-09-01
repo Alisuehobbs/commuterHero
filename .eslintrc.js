dc9c0d5219ae38f03e95d7dc32595d32c07b11c0
Ali Hobbs <alisuehobbs@gmail.com>
2016-09-01 16:11:40 -0600

    everything works and it's all linted and cleaned up

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,10 +1,13 @@
 module.exports = {
   extends: [
     'ryansobol/browser',
-    'ryansobol/es6',
+    'ryansobol/es5',
     'ryansobol/jquery'
-  ]
- //  rules: {
- //   "prefer-arrow-callback": "off",
- // }
+  ],
+  rules: {
+   "prefer-arrow-callback": "off",
+   "max-statements": "off",
+   "max-len": "off",
+   "no-alert": "off",
+ }
 };
--- app.js
+++ app.js
@@ -1,139 +1,151 @@
 $(document).ready(function() {
 
-    // modals
-    $('.modal-trigger').leanModal();
+  // modals
+  $('.modal-trigger').leanModal();
 
-    //form select
-    $('select').material_select();
+  // form select
+  $('select').material_select();
 
     // submit
-    $('#submit').on('click', function() {
-        //clear directions
-        $('#directions').children().remove()
+  $('#submit').on('click', function() {
+    // clear directions
+    $('#directions').children().remove();
 
-        var startVal = $('#start').val()
-        var endVal = $('#end').val()
-        var days = $('#days').val()
-        var typeOfTranspo = $('#typeOfTranspo').val()
+    // set variables from form input
+    var startVal = $('#start').val();
+    var endVal = $('#end').val();
+    var days = $('#days').val();
+    var typeOfTranspo = $('#typeOfTranspo').val();
 
-        //errors
-        var errors = []
-        if (startVal == "") {
-          errors.push('Please enter a valid home address.')
+    // errors
+    var errors = [];
+
+    if (startVal === '') {
+      errors.push('Please enter a valid home address.');
+    }
+    if (endVal === '') {
+      errors.push('Please enter a vaild work or school address.');
+    }
+    if (days < 1 || days > 30) {
+      errors.push('Please select a number of days between 1 and 30.');
+    }
+    if (typeOfTranspo === '') {
+      errors.push('Please select one type of transportation.');
+    }
+
+    // check for errors
+    if (errors.length !== 0) {
+      alert(errors);
+    }
+    else {
+      $('#map').show();
+      $('#showDirections').show();
+      $('.hiddenCards').show();
+      $('#hideDirections').hide();
+
+      $('#showDirections').click(function() {
+        $('#directions').show();
+        $('#showDirections').hide();
+        $('#hideDirections').show();
+      });
+
+      $('#hideDirections').click(function() {
+        $('#directions').hide();
+        $('#showDirections').show();
+        $('#hideDirections').hide();
+      });
+
+      // stats info
+      $('#statsInfo').text('If you travel by ' + typeOfTranspo + ' for ' + days + ' days this month, you will save:');
+
+        // map and directions
+      function initMap() {
+        var map = new google.maps.Map(document.getElementById('map'), {
+          zoom: 10,
+          center: {
+            lat: 40.02,
+            lng: 105.27
+          } // centralized on Boulder
+        });
+
+        var directionsService = new google.maps.DirectionsService;
+        var directionsDisplay = new google.maps.DirectionsRenderer({
+          draggable: true,
+          map: map,
+          panel: document.getElementById('directions')
+        });
+
+        directionsDisplay.addListener('directions_changed', function() {
+          computeTotalDistance(directionsDisplay.getDirections());
+        });
+
+        displayRoute(startVal, endVal, directionsService,
+          directionsDisplay);
+      }
+      initMap();
+
+      function displayRoute(origin, destination, service, display) {
+        service.route({
+          origin: origin,
+          destination: destination,
+          travelMode: typeOfTranspo
+        }, function(response, status) {
+          if (status === 'OK') {
+            display.setDirections(response);
+          }
+          else {
+            alert('Could not display directions due to: ' + status);
+          }
+        });
+      }
+
+      function computeTotalDistance(result) {
+        var total = 0;
+        var myroute = result.routes[0];
+
+        for (var i = 0; i < myroute.legs.length; i++) {
+          total += myroute.legs[i].distance.value;
         }
-        if (endVal == "") {
-          errors.push('Please enter a vaild work or school address.')
-        }
-        if (days < 1 || days > 30) {
-          errors.push('Please select a number of days between 1 and 30.')
-        }
-        if (typeOfTranspo == '') {
-          errors.push('Please select one type of transportation.')
-        }
 
-        if (errors.length !== 0) {
-          alert(errors)
-        } else {
+        var miles = ((total / 1609.344) * 2).toFixed(2);
 
-        $('#map').show()
-        $('#showDirections').show()
-        $('.hiddenCards').show()
-        $('#hideDirections').hide()
+        $('#distance').text(miles);
 
-        $('#showDirections').click(function() {
-            $('#directions').show();
-            $('#showDirections').hide();
-            $('#hideDirections').show();
-        })
+        function statCalculation(typeOfTranspo) {
+          if (typeOfTranspo === 'TRANSIT') {
+            var busGallons = ((miles / 3.26) / 35);
 
-        $('#hideDirections').click(function() {
-            $('#directions').hide();
-            $('#showDirections').show();
-            $('#hideDirections').hide();
-        })
+            var gallons = (((miles / 21.6) - busGallons) * days).toFixed(2);
 
-        //stats info
-        $('#statsInfo').text(`If you travel by ${typeOfTranspo} for ${days} days this month, you will save:`)
+            $('#gallons').text(gallons);
 
-        //map and directions
-        function initMap() {
-            var map = new google.maps.Map(document.getElementById('map'), {
-                zoom: 10,
-                center: {
-                    lat: 40.02,
-                    lng: 105.27
-                } // centralized on Boulder
-            });
+            var dollars = (gallons * 2.218).toFixed(2);
 
-            var directionsService = new google.maps.DirectionsService;
-            var directionsDisplay = new google.maps.DirectionsRenderer({
-                draggable: true,
-                map: map,
-                panel: document.getElementById('directions')
-            });
+            $('#dollars').text('$' + dollars);
 
-            directionsDisplay.addListener('directions_changed', function() {
-                computeTotalDistance(directionsDisplay.getDirections());
-            });
+            var emissions = (gallons * 8887).toFixed(2);
 
-            displayRoute(startVal, endVal, directionsService,
-                directionsDisplay);
-        }
-        initMap()
+            $('#emissions').text(emissions);
+          }
+          else {
+            var gallons = ((miles / 21.6) * days).toFixed(2);
 
-        function displayRoute(origin, destination, service, display) {
-            service.route({
-                origin: origin,
-                destination: destination,
-                travelMode: typeOfTranspo,
-            }, function(response, status) {
-                if (status === 'OK') {
-                    display.setDirections(response);
-                    console.log(response);
-                } else {
-                    alert('Could not display directions due to: ' + status);
-                }
-            });
-        }
+            $('#gallons').text(gallons);
 
-        function computeTotalDistance(result) {
-            var total = 0;
-            var myroute = result.routes[0];
-            for (var i = 0; i < myroute.legs.length; i++) {
-                total += myroute.legs[i].distance.value;
-            }
+            var dollars = (gallons * 2.218).toFixed(2);
 
-            miles = ((total / 1609.344) * 2).toFixed(2);
-            $('#distance').text(miles)
+            $('#dollars').text('$' + dollars);
 
-            function statCalculation(typeOfTranspo) {
-                if (typeOfTranspo === 'TRANSIT') {
-                    busGallons = ((miles / 3.26) / 35)
-                    gallons = (((miles / 21.6) - busGallons) * days).toFixed(2)
-                    $('#gallons').text(gallons)
+            var emissions = (gallons * 8887).toFixed(2);
 
-                    dollars = (gallons * 2.218).toFixed(2)
-                    $('#dollars').text('$' + dollars)
-
-                    emissions = (gallons * 8887).toFixed(2)
-                    $('#emissions').text(emissions)
-
-                } else {
-                    gallons = ((miles / 21.6) * days).toFixed(2)
-                    $('#gallons').text(gallons)
-
-                    dollars = (gallons * 2.218).toFixed(2)
-                    $('#dollars').text('$' + dollars)
-
-                    emissions = (gallons * 8887).toFixed(2)
-                    $('#emissions').text(emissions)
-                }
-            }
-            statCalculation(typeOfTranspo)
+            $('#emissions').text(emissions);
           }
         }
-        //scroll to stats
-        window.scroll( 0, 650 )
-    })
+        statCalculation(typeOfTranspo);
+      }
+    }
 
-})
+    // scroll to stats
+    window.scroll(0, 650);
+  });
+});
